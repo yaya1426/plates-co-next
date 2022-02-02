e529e257f112368c08834245871ffa7f617cf924
Yahya Elaraby <yaya1426@gmail.com>
2022-02-02 02:00:01 +0200

    Add bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+//Added Bootstrap styles
+import 'bootstrap/dist/css/bootstrap.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </Head>
   return <Component {...pageProps} />
 }
 
